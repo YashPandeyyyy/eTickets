70eb88a08fba1a2f388f19f4439cb8995f9e1a25
ADMIN <ADMIN@DESKTOP-33F55A5>
2025-04-10 14:42:58 +0530

    Add Dbcontext file

---
+++ eTickets/Data/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options) 
+        {
+            
+        }
+
+
+    }
+}
