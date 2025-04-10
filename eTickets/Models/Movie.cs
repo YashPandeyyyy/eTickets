09fae7742759f57270de8a86eae3de8f76865578
ADMIN <ADMIN@DESKTOP-33F55A5>
2025-04-10 14:39:52 +0530

    Add Models

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+       public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+     
+    }
+}
