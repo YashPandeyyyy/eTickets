f6b2b310cb966e8c0bb420de99c81804a76855ea
ADMIN <ADMIN@DESKTOP-33F55A5>
2025-04-10 14:43:42 +0530

    Adding Relationships to DbContext

--- eTickets/Data/AddDbContext.cs
+++ eTickets/Data/AddDbContext.cs
@@ -10,6 +10,23 @@ namespace eTickets.Data
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(am => am.ActorId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(am => am.MovieId);
 
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
