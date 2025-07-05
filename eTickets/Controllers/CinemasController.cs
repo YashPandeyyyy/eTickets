6d8b7b4fc35264e9fab2e93158480d2acfb36c0e
ADMIN <ADMIN@DESKTOP-33F55A5>
2025-07-05 20:40:24 +0530

    Add cinemas controller

---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AddDbContext _context;
+
+        public CinemasController(AddDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync(); // Fetch all cinemas from the database and store it in allCinemas variable
+            return View();
+        }
+    }
+}
