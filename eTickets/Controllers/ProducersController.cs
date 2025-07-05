0296ad31e7dc5512f1261010177b7445ece14cab
ADMIN <ADMIN@DESKTOP-33F55A5>
2025-07-05 20:36:47 +0530

    Add producers controller

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AddDbContext _context;
+
+        public ProducersController(AddDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync(); // Fetch all producers from the database and store it in allProducers variable
+            return View();
+        }
+    }
+}
